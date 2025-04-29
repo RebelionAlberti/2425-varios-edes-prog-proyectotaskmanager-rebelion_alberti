10e994b68033e38ac5fe952da8e40cc3b7b83786
jesuuslopeez <jlopezp@iesrafaelalberti.es>
2025-04-29 13:45:50 +0200

    Dashboard tareas

--- src/main/kotlin/presentacion/UI.kt
+++ src/main/kotlin/presentacion/UI.kt
@@ -1,13 +1,14 @@
 package org.practicatrim2.presentacion.presentacion
 
 import aplicacion.ActividadService
-
+import org.practicatrim2.presentacion.aplicacion.Dashboard
 
 class UI {
     companion object {
         private const val CREAR_TAREA = "1"
         private const val CREAR_EVENTO = "2"
         private const val LISTAR_ACTIVIDADES = "3"
+        private const val VER_DASHBOARD = "5"
         private const val SALIR = "4"
     }
 
@@ -20,12 +21,14 @@ class UI {
             println("2 | Agregar Evento")
             println("3 | Listar Actividades")
             println("4 | Salir")
+            println("5 | Ver Dashboard") // Nueva opción
             print("Selecciona una opción: ")
 
             when (readln()) {
                 CREAR_TAREA -> agregarTarea(servicio)
                 CREAR_EVENTO -> agregarEvento(servicio)
                 LISTAR_ACTIVIDADES -> listarActividades(servicio)
+                VER_DASHBOARD -> verDashboard(servicio)
                 SALIR -> {
                     println("Saliendo...")
                     seguir = false
@@ -63,5 +66,12 @@ class UI {
         } else {
             actividades.forEach { println(it) }
         }
+    }
+
+    private fun verDashboard(servicio: ActividadService) {
+        println("\n=== Dashboard ===")
+        val actividades = servicio.obtenerActividades()
+        val dashboard = Dashboard()
+        dashboard.mostrarResumen(actividades)
     }
 }
