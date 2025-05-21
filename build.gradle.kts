e9ca6a751d101a3d6868d0649c193d343fa56a52
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2025-05-21 16:42:37 +0200

    2 code Smells encontrados sin arreglar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "1.9.0" // o el que uses
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
 }
 
 group = "org.example"
